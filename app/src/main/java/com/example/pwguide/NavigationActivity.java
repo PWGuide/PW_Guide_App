88db6749cfbeb342447eda909760deabadf718b9
Agata Lachowiecka <agatalachowiecka@gmail.com>
2021-12-16 00:56:24 +0100

    Revert "Dodanie ekranu nawigacji v1"
    This reverts commit ce21101562851b62b488c9d27e49868e77f7f82d.

--- app/src/main/java/com/example/pwguide/NavigationActivity.java
+++ app/src/main/java/com/example/pwguide/NavigationActivity.java
@@ -3,14 +3,6 @@ package com.example.pwguide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.ImageButton;
-
-import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class NavigationActivity extends AppCompatActivity {
 
@@ -19,24 +11,5 @@ public class NavigationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
         getSupportActionBar().setTitle("Nawigacja");
-        View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        decorView.setSystemUiVisibility(uiOptions);
-
-        BottomSheetBehavior<View> sheetBehavior = BottomSheetBehavior.from(findViewById(R.id.sheet_navigation));
-        sheetBehavior.setPeekHeight(300);
-        sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
-
-        ImageButton backButton = findViewById(R.id.back_navigation);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
     }
 }
