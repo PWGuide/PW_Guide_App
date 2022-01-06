814888d7dfe6be630ae497052f17d6c807fbfc83
Olakow <ola.kowalczyk@opoczta.pl>
2022-01-07 00:15:48 +0100

    add timetable

--- app/src/main/java/com/example/pwguide/TimetableActivity.java
+++ app/src/main/java/com/example/pwguide/TimetableActivity.java
@@ -1,15 +1,80 @@
 package com.example.pwguide;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.viewpager.widget.ViewPager;
+
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.pwguide.adapters.FragmentsTabAdapter;
+import com.example.pwguide.fragments.FridayFragment;
+import com.example.pwguide.fragments.MondayFragment;
+import com.example.pwguide.fragments.SaturdayFragment;
+import com.example.pwguide.fragments.SundayFragment;
+import com.example.pwguide.fragments.ThursdayFragment;
+import com.example.pwguide.fragments.TuesdayFragment;
+import com.example.pwguide.fragments.WednesdayFragment;
+import com.example.pwguide.utils.AlertDialogsHelper;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.Calendar;
 
 public class TimetableActivity extends AppCompatActivity {
+
+
+    private FragmentsTabAdapter adapter;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timetable);
-        getSupportActionBar().setTitle("Plan zajęć");
+        getSupportActionBar().setTitle("Plaan zajęć");
+        initAll();
+    }
+
+    private void initAll() {
+        setupFragments();
+        setupCustomDialog();
+
+    }
+
+    private void setupFragments() {
+        adapter = new FragmentsTabAdapter(getSupportFragmentManager());
+        viewPager = findViewById(R.id.viewPager);
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+        adapter.addFragment(new MondayFragment(), getResources().getString(R.string.monday));
+        adapter.addFragment(new TuesdayFragment(), getResources().getString(R.string.tuesday));
+        adapter.addFragment(new WednesdayFragment(), getResources().getString(R.string.wednesday));
+        adapter.addFragment(new ThursdayFragment(), getResources().getString(R.string.thursday));
+        adapter.addFragment(new FridayFragment(), getResources().getString(R.string.friday));
+        adapter.addFragment(new SaturdayFragment(), getResources().getString(R.string.saturday));
+        adapter.addFragment(new SundayFragment(), getResources().getString(R.string.sunday));
+        viewPager.setAdapter(adapter);
+        viewPager.setCurrentItem(day == 1 ? 6 : day - 2, true);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+
+    private void setupCustomDialog() {
+        final View alertLayout = getLayoutInflater().inflate(R.layout.dialog_add_subject, null);
+        AlertDialogsHelper.getAddSubjectDialog(TimetableActivity.this, alertLayout, adapter, viewPager);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/com/example/pwguide/utils/DbHelper.java
@@ -0,0 +1,117 @@
+package com.example.pwguide.utils;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.pwguide.model.Week;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ulan on 07.09.2018.
+ */
+public class DbHelper extends SQLiteOpenHelper {
+
+    private static final int DB_VERSION = 6;
+    private static final String DB_NAME = "timetabledb";
+    private static final String TIMETABLE = "timetable";
+    private static final String WEEK_ID = "id";
+    private static final String WEEK_SUBJECT = "subject";
+    private static final String WEEK_FRAGMENT = "fragment";
+    private static final String WEEK_ROOM = "room";
+    private static final String WEEK_FROM_TIME = "fromtime";
+    private static final String WEEK_TO_TIME = "totime";
+    private static final String WEEK_COLOR = "color";
+
+
+
+    public DbHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_TIMETABLE = "CREATE TABLE " + TIMETABLE + "("
+                + WEEK_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + WEEK_SUBJECT + " TEXT,"
+                + WEEK_FRAGMENT + " TEXT,"
+                + WEEK_ROOM + " TEXT,"
+                + WEEK_FROM_TIME + " TEXT,"
+                + WEEK_TO_TIME + " TEXT,"
+                + WEEK_COLOR + " INTEGER" + ")";
+
+
+        db.execSQL(CREATE_TIMETABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        switch (oldVersion) {
+            case 1:
+                db.execSQL("DROP TABLE IF EXISTS " + TIMETABLE);
+
+                break;
+        }
+        onCreate(db);
+    }
+
+    /**
+     * Methods for Week fragments
+     **/
+    public void insertWeek(Week week) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(WEEK_SUBJECT, week.getSubject());
+        contentValues.put(WEEK_FRAGMENT, week.getFragment());
+        contentValues.put(WEEK_ROOM, week.getRoom());
+        contentValues.put(WEEK_FROM_TIME, week.getFromTime());
+        contentValues.put(WEEK_TO_TIME, week.getToTime());
+        contentValues.put(WEEK_COLOR, week.getColor());
+        db.insert(TIMETABLE, null, contentValues);
+        db.update(TIMETABLE, contentValues, WEEK_FRAGMENT, null);
+        db.close();
+    }
+
+    public void deleteWeekById(Week week) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TIMETABLE, WEEK_ID + " = ? ", new String[]{String.valueOf(week.getId())});
+        db.close();
+    }
+
+    public void updateWeek(Week week) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(WEEK_SUBJECT, week.getSubject());
+        contentValues.put(WEEK_ROOM, week.getRoom());
+        contentValues.put(WEEK_FROM_TIME, week.getFromTime());
+        contentValues.put(WEEK_TO_TIME, week.getToTime());
+        contentValues.put(WEEK_COLOR, week.getColor());
+        db.update(TIMETABLE, contentValues, WEEK_ID + " = " + week.getId(), null);
+        db.close();
+    }
+
+    public ArrayList<Week> getWeek(String fragment) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ArrayList<Week> weeklist = new ArrayList<>();
+        Week week;
+        Cursor cursor = db.rawQuery("SELECT * FROM ( SELECT * FROM " + TIMETABLE + " ORDER BY " + WEEK_FROM_TIME + " ) WHERE " + WEEK_FRAGMENT + " LIKE '" + fragment + "%'", null);
+        while (cursor.moveToNext()) {
+            week = new Week();
+            week.setId(cursor.getInt(cursor.getColumnIndex(WEEK_ID)));
+            week.setSubject(cursor.getString(cursor.getColumnIndex(WEEK_SUBJECT)));
+            week.setRoom(cursor.getString(cursor.getColumnIndex(WEEK_ROOM)));
+            week.setFromTime(cursor.getString(cursor.getColumnIndex(WEEK_FROM_TIME)));
+            week.setToTime(cursor.getString(cursor.getColumnIndex(WEEK_TO_TIME)));
+            week.setColor(cursor.getInt(cursor.getColumnIndex(WEEK_COLOR)));
+            weeklist.add(week);
+        }
+        return weeklist;
+    }
+
+
+
+}
