78fd6419ae2de13ae0b70698e576e066cd5d2f46
luizalouise <lkluiza12@gmail.com>
2022-04-05 16:06:17 +0200

    Naprawienie działanie nawigacji kiedy GPS jest wyłączony

--- app/src/main/java/com/example/pwguide/OutsideNavigation.java
+++ app/src/main/java/com/example/pwguide/OutsideNavigation.java
@@ -1,9 +1,15 @@
 package com.example.pwguide;
 
+import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -17,56 +23,29 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
-import android.os.Build;;
+;
 import android.widget.Toast;
 
 import com.example.pwguide.navigation.Building;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
+;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class OutsideNavigation extends AppCompatActivity {
+public class OutsideNavigation extends AppCompatActivity implements LocationListener {
 
-    private static final int PERMISSIONS_FINE_LOCATION = 99;
     Button b_select_plan;
     Button b_rozpocznij;
-    String lat, lon, altitude, accuracy, speed, address;
-    String source_address, dest_address;
-
-    LocationRequest locationRequest;
-    LocationCallback locationCallBack;
-    FusedLocationProviderClient fusedLocationProviderClient;
-
     ArrayList<Building> buildings = new ArrayList<>();
+    private static final int REQUEST_LOCATION = 1;
+    LocationManager locationManager;
+    String source_latitude, source_longitude;
+    String dest_latitude, dest_longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_outside_navigation);
-
-        locationRequest = LocationRequest.create();
-        locationRequest.setInterval(30000);
-        locationRequest.setFastestInterval(5000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        updateGPS();
-
-        locationCallBack = new LocationCallback(){
-
-            @Override
-            public void onLocationResult(@NonNull LocationResult locationResult) {
-                super.onLocationResult(locationResult);
-                updateUIValues(locationResult.getLastLocation());
-            }
-        };
 
         b_select_plan = findViewById(R.id.b_select_plan3);
         b_select_plan.setOnClickListener(new View.OnClickListener() {
@@ -89,17 +68,14 @@ public class OutsideNavigation extends AppCompatActivity {
         }
         //String[] buildings = getResources().getStringArray(R.array.buildings);
         String[] halls = getResources().getStringArray(R.array.halls);
-        //getSupportActionBar().setTitle("Plan");
+
         AutoCompleteTextView editText1 = findViewById(R.id.build_list3);
         ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, build_name);
         editText1.setAdapter(adapter1);
-        //editText.setThreshold(1);
-        //String input = editText.getText().toString();
-        //getSupportActionBar().setTitle("Plan");
+
         AutoCompleteTextView editText2 = findViewById(R.id.hall_list3);
         ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, halls);
         editText2.setAdapter(adapter2);
-
 
         ImageView arrow1 = (ImageView)findViewById(R.id.drop_down);
         ImageView arrow2 = (ImageView)findViewById(R.id.drop_down2);
@@ -121,107 +97,82 @@ public class OutsideNavigation extends AppCompatActivity {
             }
         });
 
+        ActivityCompat.requestPermissions( this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
         b_rozpocznij = findViewById(R.id.b_rozpocznij2);
         b_rozpocznij.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                startLocationUpdates();
-                String building = editText2.getText().toString();
 
+                String building = build_list.getText().toString();
+                System.out.println(building);
                 int index = 0;
                 for(int i = 0; i < buildings.size(); i++) {
                     if (buildings.get(i).getName().equals(building)) {
+                        System.out.println(buildings.get(i).getName());
                         index = i;
                     }
                 }
-                source_address = address;
-                dest_address = buildings.get(index).getName();
-                DisplayTrack(source_address.trim(),dest_address.trim());
-                //Toast.makeText(getBaseContext(), "Szerokość geograficzna: "+ lat + " długość: " + lon + " Adres: " + address , Toast.LENGTH_SHORT ).show();
+                dest_latitude = String.valueOf(buildings.get(index).getLatitude());
+                dest_longitude = String.valueOf(buildings.get(index).getLongitude());
+
+                locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    OnGPS();
+                } else {
+                    getLocation(dest_latitude, dest_longitude);
+                }
             }
         });
-
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        switch (requestCode){
-            case PERMISSIONS_FINE_LOCATION:
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    updateGPS();
-                }
-                else{
-                    Toast.makeText(this, "Ta aplikacja wymaga zezwolenia, aby mogła działać poprawnie", Toast.LENGTH_SHORT).show();
-                    finish();
-                }
-                break;
-        }
     }
 
-    private void startLocationUpdates() {
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
-            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallBack, null);
-            updateGPS();
-        }
+    private void OnGPS() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Udziel dostępu do GPSa").setCancelable(false).setPositiveButton("Tak", new  DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        }).setNegativeButton("Nie", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
-    private void updateGPS(){
+    private void getLocation(String dest_latitude, String dest_longitude) {
+        if (ActivityCompat.checkSelfPermission(
+                OutsideNavigation.this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                OutsideNavigation.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        } else {
+            locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, (LocationListener) this, null);
+            Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(OutsideNavigation.this);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ){
-            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    updateUIValues(location);
+            if(locationGPS == null){
+                locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, this, null);
+                locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            }
 
-                }
-            });
-        }
-        else{
-            if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.M){
-                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},PERMISSIONS_FINE_LOCATION);
+            if (locationGPS != null) {
+                double lat = locationGPS.getLatitude();
+                double longi = locationGPS.getLongitude();
+                source_latitude = String.valueOf(lat);
+                source_longitude = String.valueOf(longi);
+                //Toast.makeText(this,"Twoja lokalizacja: " + "\n" + "Latitude: " + source_latitude + "\n" + "Longitude: " + source_longitude, Toast.LENGTH_SHORT).show();
+                DisplayTrack(source_latitude, source_longitude, dest_latitude, dest_longitude);
+            } else {
+                Toast.makeText(this, "Nie można znaleźć twojej lokalizacji.", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    private void updateUIValues(Location location){
-        lat = String.valueOf(location.getLatitude());
-        lon = String.valueOf(location.getLongitude());
-        accuracy = String.valueOf(location.getAccuracy());
-
-        if (location.hasAltitude()){
-            altitude = String.valueOf(location.getAltitude());
-        }
-        else{
-            altitude = "Niedostępne";
-        }
-
-        if (location.hasSpeed()){
-            speed = String.valueOf(location.getSpeed());
-        }
-        else{
-            speed = "Niedostępne";
-        }
-
-        Geocoder geocoder = new Geocoder(OutsideNavigation.this);
-
+    private void DisplayTrack(String source_latitude, String source_longitude, String dest_latitude, String dest_longitude){
         try{
-            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-            address = addresses.get(0).getAddressLine(0);
-
-        }
-        catch(Exception e){
-            address = "Nie można uzyskać adresu ulicy";
-        }
-
-    }
-
-    private void DisplayTrack(String sSource, String sDestination){
-        try{
-            Uri uri = Uri.parse("https://www.google.co.in/maps/dir/" + sSource + "/" + sDestination);
+            Uri uri = Uri.parse("http://maps.google.com/maps?saddr=" + source_latitude +"," + source_longitude + "&daddr=" + dest_latitude +"," + dest_longitude);
             Intent intent = new Intent(Intent.ACTION_VIEW, uri);
             intent.setPackage("com.google.android.apps.maps");
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -233,5 +184,30 @@ public class OutsideNavigation extends AppCompatActivity {
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        double lat = location.getLatitude();
+        double longi = location.getLongitude();
+        source_latitude = String.valueOf(lat);
+        source_longitude = String.valueOf(longi);
+        //Toast.makeText(this,"Twoja lokalizacja: " + "\n" + "Latitude: " + source_latitude + "\n" + "Longitude: " + source_longitude, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
     }
 }
