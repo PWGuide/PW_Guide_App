c1721d9d58c5adacc38d4cb33115dae712aca85c
Kinga <kocol.kinga@gmail.com>
2022-06-04 21:02:27 +0200

    podłączenie sieci do aplikacji (VGG16)

--- app/src/main/java/com/example/pwguide/AlertActivity.java
+++ app/src/main/java/com/example/pwguide/AlertActivity.java
@@ -2,31 +2,26 @@ package com.example.pwguide;
 
 import android.content.DialogInterface;
 import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class AlertActivity {
-    public void createAlert(AlertDialog.Builder builder) {
+public class AlertActivity extends AppCompatActivity {
+    public void createAlert(AlertDialog.Builder builder, String title, String short_txt, String long_txt) {
 
-        builder.setTitle("Gmach Główny Politechniki Warszawskiej");
-        builder.setMessage("zabytkowy budynek Politechniki Warszawskiej znajdujący się na placu Politechniki w Warszawie.");
+        builder.setTitle(title);
+        builder.setMessage(short_txt);
         builder.setCancelable(true);
-        builder.setPositiveButton("MORE INFO", new DialogInterface.OnClickListener() {
+        builder.setPositiveButton("Więcej", new DialogInterface.OnClickListener() {
 
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                builder.setTitle("Gmach Główny Politechniki Warszawskiej");
-                builder.setMessage("Gmach powstał na zadrzewionym skwerze, na którym m.in. w 1896 odbyła się wystawa higieniczna[1]. Budowa gmachu zaprojektowanego dla Instytutu Politechnicznego im. Cara Mikołaja II przez Stefana Szyllera rozpoczęła się w lipcu 1899 roku i trwała 2 lata.\n" +
-                        "\n" +
-                        "Gmach jest typowym przykładem architektury odwołującej się do stylistyki włoskiego renesansu i baroku. Jego reprezentacyjny charakter podkreśla monumentalna, dwukondygnacyjna fasada o zaokrąglonych narożnikach. Gmach został zaprojektowany według klasycznych reguł przyjmowanych przez ówczesne uczelnie w Europie. Masywne mury nośne budynku stanowiły nie tylko konstrukcję podpierającą stropy i dach, ale kryły w sobie sieć kanałów dawnego systemu ogrzewania ciepłym powietrzem doprowadzanym z kotłowni zewnętrznej. Solidna konstrukcja przetrwała pożary i zniszczenia od pocisków z czasu powstania warszawskiego.\n" +
-                        "\n" +
-                        "Projektując elewacje Gmachu Głównego, Stefan Szyller najwięcej starań poświęcił ukształtowaniu fasady o wspaniałej ekspozycji od strony placu. Według zasady architecture parlante XIX-wieczne dekoracje budynków stylu wysokiego: rzeźby i płaskorzeźby, malarstwo, freski i sgraffito - przedstawienia alegoryczne i symboliczne, podkreślały, przede wszystkim jednak uwznioślały, przeznaczenie, funkcje i charakter gmachów publicznych.\n" +
-                        "\n" +
-                        "W 2005, dla upamiętnienia nadania doktoratu honoris causa Politechniki (1926), w gmachu odsłonięto pomnik Marii Skłodowskiej-Curie zaprojektowany przez Maksymiliana Biskupskiego[2]. ");
+                builder.setTitle(title);
+                builder.setMessage(long_txt);
                 builder.setCancelable(false);
-                builder.setPositiveButton("OK", null);
+                builder.setPositiveButton("Ok", null);
                 builder.create().show();
             }
 
-        }).setNegativeButton("Cancel", null);
+        }).setNegativeButton("Zamknij", null);
 
         builder.create().show();
     }
--- app/src/main/java/com/example/pwguide/MenuActivity.java
+++ app/src/main/java/com/example/pwguide/MenuActivity.java
@@ -4,15 +4,27 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+
+import org.datavec.image.loader.NativeImageLoader;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.util.ModelSerializer;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
+import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -21,7 +33,15 @@ public class MenuActivity extends AppCompatActivity {
     Button btn_navig;
     Button btn_info;
 
+    MultiLayerNetwork model;
+
+    InputStream inputStream;
+
     AlertActivity alert = new AlertActivity();
+
+    String[] titles;
+    String[] short_texts;
+    String[] long_texts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,16 +59,28 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
+        inputStream = getResources().openRawResource(R.raw.trained_model_vgg16_6);
+
+        titles = getResources().getStringArray(R.array.titles_array);
+        short_texts = getResources().getStringArray(R.array.short_texts_array);
+        long_texts = getResources().getStringArray(R.array.long_texts_array);
+
+        {
+            try {
+                model = ModelSerializer.restoreMultiLayerNetwork(inputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         btn_camera = findViewById(R.id.btn_camera);
         btn_camera.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MenuActivity.this, CameraActivity.class);
-                startActivity(intent);
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 101);
             }
-
         });
 
         btn_navig = findViewById(R.id.btn_navig);
@@ -73,4 +105,56 @@ public class MenuActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+
+            NativeImageLoader loader = new NativeImageLoader(150, 150, 3);
+
+            INDArray image = null;
+
+            try {
+                image = loader.asMatrix(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            DataNormalization scalar = new ImagePreProcessingScaler(0, 1);
+            scalar.transform(image);
+
+            INDArray output = model.output(image);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(MenuActivity.this);
+
+            int index;
+
+            index = getIndex(output);
+
+            String title = titles[index];
+            String short_txt = short_texts[index];
+            String long_txt = long_texts[index];
+
+            alert.createAlert(builder, title, short_txt, long_txt);
+        }
+    }
+
+    int getIndex(INDArray output) {
+        int index = 0;
+        double max = output.getDouble(0);
+
+        for (int i=1; i < output.length(); i++) {
+            if (output.getDouble(i) > max) {
+                max = output.getDouble(i);
+                index = i;
+            }
+        }
+
+        if (max < 0.8){
+            index = 22;
+        }
+
+        return index;
+    }
 }
